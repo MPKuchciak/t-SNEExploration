71737560e0cb7ebbdd1f81a0dd9d2a6ae63a5a0c
MPKuchciak <90063362+MPKuchciak@users.noreply.github.com>
2024-11-11 20:39:43 +0100

    testing

---
+++ src/R.File/main.R
@@ -0,0 +1,8 @@
+getwd()
+# 1. PURLING Rmd files into R files ============================================
+# In this section we can transform the Rmd files into R files. 
+# An important part of the transformation is the `documentation` parameter.
+# Value `2` will include text in the resulting `R` files as  `roxygen2` comments.
+knitr::purl(input    = "src/Dimension_reduction.Rmd",
+            output   = "src/R.File/Dimension_reduction.R",
+            documentation = 2)
